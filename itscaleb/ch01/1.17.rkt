dc832475eae211243f12e30858f18c31476374e2
Caleb Gossler <caleb.gossler@gmail.com>
2014-06-07 23:14:11 -0700

    finished 1.17

---
+++ itscaleb/ch01/1.17.rkt
@@ -0,0 +1,22 @@
+#lang racket
+(require rackunit)
+
+(define (my* x y)
+  (mult-itr x y 0))
+
+(define (mult-itr x y a)
+  (cond 
+    ((= y 1) (+ x a))
+    ((even? y)
+     (mult-itr (double x) (halve y) a))
+    (else
+     (mult-itr x (- y 1) (+ x a)))))
+
+(define (double a) (+ a a))
+
+(define (halve a) (/ a 2))
+
+(check-equal? (my* 2 2) 4)
+(check-equal? (my* 2 4) 8)
+(check-equal? (my* 3 3) 9)
+(check-equal? (my* 6 4) 24)
