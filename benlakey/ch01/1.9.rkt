827ea88114fd01d3e60e9cee0887274ea864f8c1
Ben Lakey <benlakey@gmail.com>
2014-06-08 21:27:57 -0700

    Updated 1.9

--- benlakey/ch01/1.9.rkt
+++ benlakey/ch01/1.9.rkt
@@ -1,10 +1,15 @@
 #lang racket
 
-(define (inc x)
-  (+ x 1))
+; Originally I had implemented 'inc' and 'dec' to actually run the sample 
+; procs in 1.9. Unfortunately in doing so I used the '+' operator which we 
+; then promptly overloaded below. This led to infinite recursion :(. I am 
+; leaving these here as a reminder of this.
 
-(define (dec x)
-  (- x 1))
+;(define (inc x)
+;  (- x 1))
+
+;(define (dec x)
+;  (- x 1))
 
 ; Variant #1
 
@@ -25,25 +30,16 @@
 (inc (inc (inc (inc (+ (dec 1) 5)))))
 (inc (inc (inc (inc (+ 0 5)))))
 (inc (inc (inc (inc 5))))
-(inc (inc (inc (+ 5 1))))
-(inc (inc (inc (inc (+ (dec 5) 1)))))
-(inc (inc (inc (inc (+ 4 1)))))
-(inc (inc (inc (inc (inc (+ (dec 4) 1))))))
-(inc (inc (inc (inc (inc (+ 3 1))))))
-(inc (inc (inc (inc (inc (inc (+ (dec 3) 1)))))))
-(inc (inc (inc (inc (inc (inc (+ 2 1)))))))
-(inc (inc (inc (inc (inc (inc (inc (+ (dec 2) 1))))))))
-(inc (inc (inc (inc (inc (inc (inc (+ 1 1))))))))
-(inc (inc (inc (inc (inc (inc (inc (inc (+ (dec 1) 1)))))))))
-(inc (inc (inc (inc (inc (inc (inc (inc (+ 0 1)))))))))
-(inc (inc (inc (inc (inc (inc (inc (inc 1))))))))
-(inc (inc (inc (inc (inc (inc (inc (+ 1 1))))))))
-(inc (inc (inc (inc (inc (inc (inc (inc (+ (dec 1) 1)))))))))
-; Seeing an infinite loop here ... (and verified with the REPL)
+(inc (inc (inc 6)))
+(inc (inc 7))
+(inc 8)
+9
 
 ; This is not iterative because we don't carry forward with us a 
 ; complete picture of the program state. We are relying on the 
 ; preceding calls in the stack to continue other calculations.
+; The shape of the program execution is also a strong suggestion 
+; that its recursive; it expands and contracts.
 
 
 ; Variant #2
@@ -54,21 +50,19 @@
       (+ (dec a) (inc b))))
 
 ; (+ 4 5) would substitute as follows:
-; a=4, b=5
-
 (+ 4 5)
 (+ (dec 4) (inc 5))
-(+ 3 (+ 5 1))
-(+ 3 (+ (dec 5) (inc 1)))
-(+ 3 (+ 4 (+ 1 1)))
-(+ 3 (+ 4 (+ (dec 1) (inc 1))))
-(+ 3 (+ 4 (+ 0 (+ 1 1))))
-(+ 3 (+ 4 (+ (dec 1) (inc 1))))
-(+ 3 (+ 4 (+ 0 (+ 1 1))))
-; Again an infinite loop here (verified with REPL)
+(+ 3 6)
+(+ (dec 3) (inc 6))
+(+ 2 7)
+(+ (dec 2) (inc 7))
+(+ 1 8)
+(+ (dec 1) (inc 8))
+(+ 0 9)
+9
 
-; This one seems recursive as well since there are actual deferred operations here.
 
-; NOW, had we not redefined (+ a b) and used a different proc name instead, neither of 
-; these would result in an infinite loop, and the 2nd one would be iterative since 
-; there would be no deferred operations.
+; This is iterative because there are no deferred operations to 
+; complete. We can pause the computation at any point and resume 
+; based on state. The shape is also non expanding which suggests 
+; iterative.
