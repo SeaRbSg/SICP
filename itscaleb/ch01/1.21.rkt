11a2ba616d6c4059756dfdd697f7582fa2b9b728
Caleb Gossler <caleb.gossler@gmail.com>
2014-06-14 22:57:57 -0700

    finished 1.21

---
+++ itscaleb/ch01/1.21.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (square a) (* a a))
+
+(display (smallest-divisor 199))
+(newline)
+(display (smallest-divisor 1999))
+(newline)
+(display (smallest-divisor 19999))
