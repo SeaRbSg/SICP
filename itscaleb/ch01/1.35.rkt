2d1f00b16b5016b19798da5abfe0896b94d2736f
Caleb Gossler <caleb.gossler@gmail.com>
2014-07-20 00:13:37 -0700

    finished 1.35

---
+++ itscaleb/ch01/1.35.rkt
@@ -0,0 +1,19 @@
+#lang racket
+(require rackunit)
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+; check if the fixed point of x -> 1 + 1/x is the golden ratio
+(check-= (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)
+         1.618033 ; golden radio
+         .000001)
