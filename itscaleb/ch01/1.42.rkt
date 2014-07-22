e5b7f3e4d58a6d846de8c7a33559e08dc4577573
Caleb Gossler <caleb.gossler@gmail.com>
2014-07-21 22:37:01 -0700

    finished 1.42

---
+++ itscaleb/ch01/1.42.rkt
@@ -0,0 +1,12 @@
+#lang racket
+(require rackunit)
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define (square x) (* x x))
+(define (inc x) (+ 1 x))
+
+(check-equal?
+ ((compose square inc) 6)
+ 49)
