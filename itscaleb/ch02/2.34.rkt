ae3a231a381ded84d5bd13ef28b3f62320f44db1
Caleb Gossler <caleb.gossler@gmail.com>
2014-09-08 22:40:01 -0700

    2.32, Horner's rule

---
+++ itscaleb/ch02/2.34.rkt
@@ -0,0 +1,17 @@
+#lang racket
+(require rackunit)
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms)
+                (+ (* higher-terms x) this-coeff))
+              0
+              coefficient-sequence))
+
+(check-equal? (horner-eval 2 '(1 3 0 5 0 1))
+              79)
