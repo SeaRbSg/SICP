4545b16e1b93619ef4ea97a5b2b97cde9a48c807
Caleb Gossler <caleb.gossler@gmail.com>
2014-11-10 22:40:23 -0800

    cleaner solution to password protection

--- itscaleb/ch03/3.03.rkt
+++ itscaleb/ch03/3.03.rkt
@@ -10,14 +10,19 @@
   (define (deposit amount)
     (set! balance (+ balance amount))
     balance)
-  (define (dispatch secret-password m)
-    (if (not (eq? secret-password password))
-        (error "Incorrect password")
-        (cond ((eq? m 'withdraw) withdraw)
-              ((eq? m 'deposit) deposit)
-              (else (error "Unknown request -- MAKE-ACCOUNT"
-                           m)))))
-  dispatch)
+  (define (dispatch m)
+    (cond ((eq? m 'withdraw) withdraw)
+          ((eq? m 'deposit) deposit)
+          (else (error "Unknown request -- MAKE-ACCOUNT"
+                       m))))
+  (password-protect password dispatch))
+
+(define (password-protect user-password f)
+  (lambda (password m)
+    (cond [(eq? password user-password)
+           (f m)]
+          [else
+           (error "Incorrect password")])))
 
 (define account (make-account 100 'password))
 (check-equal? ((account 'password 'withdraw) 50) 50)
