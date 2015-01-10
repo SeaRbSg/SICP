d63e68d9ec12153dc09a1040cf3f5a765f4cba69
Caleb Gossler <caleb.gossler@gmail.com>
2015-01-09 22:40:38 -0800

    stream of integer pairs

---
+++ itscaleb/ch03/3.66.rkt
@@ -0,0 +1,19 @@
+#lang racket
+(require "streams.rkt")
+
+(define (pairs s t)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (interleave
+    (stream-map (lambda (x) (list (stream-car s) x))
+                (stream-cdr t))
+    (interleave
+     (stream-map (lambda (x) (list x (stream-car t)))
+                 (stream-cdr s))
+     (pairs (stream-cdr s) (stream-cdr t))))))
+
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+                   (interleave s2 (stream-cdr s1)))))
