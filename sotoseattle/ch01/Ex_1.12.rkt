28beae0334ff7fb26a5e55f35a0b156a6c625910
Javier Soto <sotoseattle@gmail.com>
2015-07-28 10:12:50 -0700

    refactor 12, rename all, pass on 13

--- sotoseattle/ch01/Ex_1.12.rkt
+++ sotoseattle/ch01/Ex_1.12.rkt
@@ -8,16 +8,18 @@
         (cons (+ (car l) (car (cdr l))) (rowdy (cdr l)))))
   (cons 1 (rowdy r)))
 
-(define (pascal n)
-  (define (pascualino r c)
-    (if (= c 0)
-        '()
-        (cons (row r) (pascualino (row r) (- c 1)))))
-  (cons '(1) (pascualino '(1) (- n 1))))
-
 [check-equal? (row '(1)) '(1 1)]
 [check-equal? (row '(1 1)) '(1 2 1)]
 [check-equal? (row '(1 2 1)) '(1 3 3 1)]
+
+(define (pascal n)
+  (define (pascualino r c)
+    (let ((new_row (row r)))
+      (if (= c 0)
+          '()
+          (cons new_row (pascualino new_row (- c 1))))))
+  (cons '(1) (pascualino '(1) (- n 1))))
+
 [check-equal? (pascal 3) '((1) (1 1) (1 2 1))]
 [check-equal? (pascal 5) '((1) (1 1) (1 2 1) (1 3 3 1) (1 4 6 4 1))]
 
---
+++ sotoseattle/ch01/Ex_1.13.rkt
@@ -0,0 +1,5 @@
+#lang racket
+(require rackunit)
+
+(pretty-print 'I_couldnt_care_less_about_a_mathematical_proof._Sorry!)
+
